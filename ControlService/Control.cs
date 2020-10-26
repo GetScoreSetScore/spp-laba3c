2d76f6ebb1b89a987308e2f9c5e077a5cbcc6497
GetScoreSetScore <andy844551@gmail.com>
2020-10-26 22:06:43 +0300

    Added servicereferences to controlservice

--- ControlService/Control.cs
+++ ControlService/Control.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
-
+using ControlService.FilterServiceReferenceCon;
+using ControlService.LoadServiceReferenceCon;
+using ControlService.MailServiceReferenceCon;
 namespace ControlService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
